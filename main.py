48796e53de234a1fbb440f5b5a2168045af2e488
Ayshwarya Jagadeesan <ayshjag@med.umich.edu>
2023-04-18 15:08:13 -0400

    made the code respond to environment variables, it accepts url as well as file location as an environment variable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI,Request
+from pydantic import BaseSettings
 from rdflib import Graph
 import pandas as pd
 from rdflib import Graph
@@ -11,48 +12,49 @@ from esteemer.esteemer import Esteemer
 from pictoralist.pictoralist import Pictoralist
 
 import json
+import requests
+from requests_file import FileAdapter
+import os
+class Settings(BaseSettings):
+    pathways: str = "file://"+os.path.abspath("startup/causal_pathways.json")
+    
+    measures: str ="file://"+os.path.abspath("startup/measure_details.json")
+    templates: str ="file://"+os.path.abspath("startup/templates.json")
+   
+se =requests.Session()
+se.mount('file://',FileAdapter())
+settings = Settings()
 app = FastAPI()
 
 measure_details=Graph()
 causal_pathways=Graph()
 templates=Graph()
-message_code=Graph()
-causal_pathways_alice=Graph()
-templates_alice=Graph()
+
+
+
+
 
 @app.on_event("startup")
 async def startup_event():
     try:
         
-        f1=open("./startup/message_code.json")
-        f2=open("./startup/causal_pathways.json")
-        f3=open("./startup/measure_details.json")
-        f4=open("./startup/templates.json")
-        # f5=open("./startup/causal_pathways_alice.json")
-        # f6=open("./startup/templates_alice.json")
-        global measure_details,message_code,causal_pathways,templates,templates_alice,causal_pathways_alice,f3json
-        f1json=json.load(f1)
-        f2json=json.load(f2)
-        f3json=json.load(f3)
-        f4json=json.load(f4)
-        # f5json=json.load(f5)
-        # f6json=json.load(f6)
+      
+        global measure_details,causal_pathways,templates,f3json
         
-        message_code=read_graph(f1json)
+        
+        f2json=se.get(settings.pathways).text
+        f3json=se.get(settings.measures).text
+        f4json=se.get(settings.templates).text
+       
+     
         causal_pathways=read_graph(f2json)
-        global templates
         templates=read_graph(f4json)
-        # causal_pathways_alice=read_graph(f5json)
-        # templates_alice=read_graph(f6json)
-        
-        
-        #base_graph=create_base_graph(message_code,causal_pathways,measure_details,templates)
-        # df=to_dataframe(base_graph)
-        # df.to_csv("basegraph.csv")
         print("startup is complete")
     except Exception as e:
         print("Looks like there is some problem in connection,see below traceback")
         raise e
+    
+
 @app.get("/")
 async def root():
     return{"Hello":"Universe"}
@@ -63,31 +65,11 @@ async def createprecisionfeedback(info:Request):
     req_info =await info.json()
     req_info1=req_info
     performance_data = req_info1["Performance_data"]
-    vignette=req_info1["vignette"]
     debug=req_info1["debug"]
     performance_data_df =pd.DataFrame (performance_data, columns = [ "Staff_Number","Measure_Name","Month","Passed_Count","Flagged_Count","Denominator","Peer_Average","Top_10_Average","Top_25_Average"])
     performance_data_df.columns = performance_data_df.iloc[0]
     performance_data_df = performance_data_df[1:]
-    #df = df.iloc[1: , :]
-    #performance_data_df.to_csv('pd.csv', index=False)
-    #print(type())
-    if str(vignette)!="base":
-        csp="./startup/"+str(vignette)+"/causal_pathways.json"
-        temp="./startup/"+str(vignette)+"/templates.json"
-        f2=open(csp)
-        f4=open(temp)
-        f2json=json.load(f2)
-        f4json=json.load(f4)
-        causal_pathways=read_graph(f2json)
-        templates=read_graph(f4json)
-    else:
-        f2=open("./startup/causal_pathways.json")
-        f4=open("./startup/templates.json")
-        f2json=json.load(f2)
-        f4json=json.load(f4)
-        causal_pathways=read_graph(f2json)
-        templates=read_graph(f4json)
-
+    
     del req_info1["Performance_data"]
     history=req_info1["History"]
     del req_info1["History"]
@@ -95,11 +77,13 @@ async def createprecisionfeedback(info:Request):
     del req_info1["Preferences"]
     input_message=read_graph(req_info1)
     measure_details= Graph()
+    
     for s,p,o in measure_details.triples((None,None,None)):
         measure_details.remove((s,p,o))
     
     measure_details=read_graph(f3json)
     performer_graph=create_performer_graph(measure_details)
+    
     #BitStomach
     bs=Bit_stomach(performer_graph,performance_data_df)
     BS=bs.annotate()
@@ -108,6 +92,7 @@ async def createprecisionfeedback(info:Request):
         f = open("outputs/spek_bs.json", "w")
         f.write(op)
         f.close()
+    
     #CandidateSmasher
     cs=CandidateSmasher(BS,templates)
     df_graph=cs.get_graph_type()
@@ -159,6 +144,7 @@ async def createprecisionfeedback(info:Request):
     # print(vignette)
     
     return {
+        # "location":settings.location
         "status":"Success",
         "selected_message": selected_message
         #   "selected_message": selected_message
