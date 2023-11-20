7c37241947dc473d0f3d6988f885faf4117d77df
mackgalante <mackgalante@gmail.com>
2023-11-20 14:40:47 -0500

    Remove error catcher for bitstomach input

--- bit_stomach/student_t_cleaner.py
+++ bit_stomach/student_t_cleaner.py
@@ -46,12 +46,12 @@ def student_t_cleaner(perf_dataframe):
 
 
 
-
+    '''
     ## Check if there are no measures with more than 3 unique months after cleaning
     # NOTE: still allows through data for measures with less than 3 months, but if selected we will only get text feedback so that should work.
-    if perf_dataframe.groupby('measure')['month'].nunique().max() <= 3:
+    if perf_dataframe.groupby('measure')['month'].nunique().max() < 3:
         logger.error(f"Not enough significant data detected in performance block, aborting feedback...")
         raise ValueError("PROCESS_ABORTED")
-
+    '''
     return perf_dataframe
 
