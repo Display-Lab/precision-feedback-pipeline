4892b7bc95ea51c1694f9366b389fa2217fa3992
Ayshwarya Jagadeesan <ayshjag@med.umich.edu>
2024-01-25 13:04:18 -0500

    added unit test to test the slope function

--- bit_stomach/tests/test_trend_annotate.py
+++ bit_stomach/tests/test_trend_annotate.py
@@ -1,14 +1,12 @@
 import unittest
-from unittest.mock import Mock
+from unittest.mock import patch,Mock
 import trend_annotate
 import pandas as pd
 import json
 import rdflib
 from rdflib import Graph, Literal, Namespace, URIRef,BNode
 from rdflib.serializer import Serializer
-from operator import itemgetter
-import itertools
-import collections
+
 
 
 
@@ -171,3 +169,89 @@ class Trend_annotateTestcase(unittest.TestCase):
             assert Expected_Trend_slope == Resultant_Trend_slope
             #assert if intervals is added correctly
             assert Resultant_Intervals == Expected_Intervals
+
+    def test_theil_reg(self):
+        #create test data
+        #test data 1
+        back_up_df= pd.DataFrame(columns=['measure', 'month', 'peer_average_comparator',
+        'peer_90th_percentile_benchmark', 'peer_75th_percentile_benchmark',
+        'Performance_Rate', 'goal_comparison_value'], 
+                    index = [1, 2, 3])
+        back_up_df.loc[1] = ["BP03","2023-10-01",85,88,96,0.85,0.90]
+        back_up_df.loc[2] = ["BP03","2023-11-01",87,88,96,0.87,0.90]
+        back_up_df.loc[3] = ["BP03","2023-12-01",90,88,96,0.90,0.90]
+
+        xcol='month' 
+        ycol='Performance_Rate'
+        back_up_df["month"]=back_up_df["month"].astype('datetime64')
+       
+        #test data 2
+        back_up_df1= pd.DataFrame(columns=['measure', 'month', 'peer_average_comparator',
+        'peer_90th_percentile_benchmark', 'peer_75th_percentile_benchmark',
+        'Performance_Rate', 'goal_comparison_value'], 
+                    index = [1, 2, 3])
+        back_up_df1.loc[1] = ["BP03","2023-10-01",85,88,96,0.85,0.90]
+        back_up_df1.loc[2] = ["BP03","2023-11-01",87,88,96,0.85,0.90]
+        back_up_df1.loc[3] = ["BP03","2023-12-01",90,88,96,0.85,0.90]
+
+        xcol='month' 
+        ycol='Performance_Rate'
+        back_up_df1["month"]=back_up_df1["month"].astype('datetime64')
+
+        #test data 3
+        back_up_df2= pd.DataFrame(columns=['measure', 'month', 'peer_average_comparator',
+        'peer_90th_percentile_benchmark', 'peer_75th_percentile_benchmark',
+        'Performance_Rate', 'goal_comparison_value'], 
+                    index = [1, 2, 3])
+        back_up_df2.loc[1] = ["BP03","2023-10-01",85,88,96,0.85,0.90]
+        back_up_df2.loc[2] = ["BP03","2023-11-01",85,88,96,0.82,0.90]
+        back_up_df2.loc[3] = ["BP03","2023-12-01",85,88,96,0.80,0.90]
+
+        xcol='month' 
+        ycol='Performance_Rate'
+        back_up_df2["month"]=back_up_df2["month"].astype('datetime64')
+        
+        #call method of test
+        out=trend_annotate.theil_reg(back_up_df,xcol,ycol)
+        trend_slope=out[0]
+
+        out1=trend_annotate.theil_reg(back_up_df1,xcol,ycol)
+        trend_slope1=out1[0]
+        # print(out1)
+
+        out2=trend_annotate.theil_reg(back_up_df2,xcol,ycol)
+        trend_slope2=out2[0]
+        # print(out2)
+
+        
+
+        #expected output
+        expected_output=9.486945962355806e-18
+        expected_output1=0
+        expected_output2=-9.486945962355786e-18
+
+        #assert whether resulted output equals expected output
+        assert trend_slope == expected_output
+        #assert second data output is correct
+        assert trend_slope1 == expected_output1
+        #asser third data output is correct
+        assert trend_slope2 == expected_output2
+
+
+
+
+    
+    
+    
+    
+    # def test_trend_annotate(self):
+    #     #create test data
+    #     #test data1
+    #     back_up_df= pd.DataFrame(columns=['measure', 'month', 'peer_average_comparator',
+    #     'peer_90th_percentile_benchmark', 'peer_75th_percentile_benchmark',
+    #     'Performance_Rate', 'goal_comparison_value'], 
+    #                 index = [1, 2, 3])
+    #     back_up_df.loc[1] = ["BP03","2023-11-01",85,88,96,0.85,0.90]
+    #     back_up_df.loc[2] = ["BP03","2023-11-01",85,88,96,0.85,0.90]
+    #     back_up_df.loc[3] = ["BP03","2023-12-01",85,88,96,0.85,0.90]
+    
