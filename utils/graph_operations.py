d913b30341d29c2930f2c557d3f599efffc3f8d1
Farid Seifi <farid.seifi@gmail.com>
2024-06-19 12:06:50 -0400

    updated the manifest_to_graph method and the readme file

--- utils/graph_operations.py
+++ utils/graph_operations.py
@@ -1,4 +1,4 @@
-from urllib.parse import urljoin, urlparse
+from urllib.parse import urljoin
 from urllib.request import urlopen
 
 import yaml
@@ -9,36 +9,27 @@ from rdflib import Graph
 def manifest_to_graph(manifest_path: str) -> Graph:
     g: Graph = Graph()
     
-    try:
-        manifest = extract_manifest(manifest_path)   
+    try:    
+        with urlopen(manifest_path) as f:
+            manifest_file = yaml.safe_load(f.read().decode("utf-8"))
     except Exception as e:
         logger.error(f"Error loading manifest: {e}")
-        return g  # Return an empty graph if the manifest cannot be loaded
+        return g  # Return an empty graph if the manifest cannot be loaded  
+          
+    for key, values in manifest_file.items():
+        base = urljoin(manifest_path, key)
+        for value in values:
+            file = urljoin(base,value)
+            try:
+                with urlopen(file) as f:
+                    file_content = f.read().decode("utf-8")
 
-    for file in manifest:
-        try:
-            with urlopen(file) as f:
-                file_content = f.read().decode("utf-8")
+                temp_graph = Graph().parse(data=file_content, format="json-ld")
+                g += temp_graph
 
-            temp_graph = Graph().parse(data=file_content, format="json-ld")
-            g += temp_graph
-
-            logger.debug(f"Graphed file {file}")
-        except Exception as e:
-            logger.error(f"Error processing item {file}: {e}")
-            continue
+                logger.debug(f"Graphed file {file}")
+            except Exception as e:
+                logger.error(f"Error processing item {file}: {e}")
+                continue   
 
     return g
-
-def extract_manifest(manifest_path: str) -> list:
-    manifest = []
-    with urlopen(manifest_path) as f:
-        manifest_file = yaml.safe_load(f.read().decode("utf-8"))
-        
-    for key, values in manifest_file.items():
-        parsed_url = urlparse(key)
-        if not parsed_url.scheme:
-            key = urljoin(manifest_path, key)
-        for value in values:
-            manifest.append(urljoin(key,value))
-    return manifest
